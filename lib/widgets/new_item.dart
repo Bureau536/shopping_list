173a47ad4e7d4ad32af3d944dd58f1a0c204455a
Bureau536 <g.bureau15@gmail.com>
2024-11-09 12:38:35 -0300

    Lesson 226
    Working with the request and waiting for the response.

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -21,12 +21,12 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https('flutter-prep-3ea0a-default-rtdb.firebaseio.com',
           'shopping-list.json');
-      http.post(
+      final response = await http.post(
         url,
         headers: {
           'Content-Type': 'application/json',
@@ -39,7 +39,14 @@ class _NewItemState extends State<NewItem> {
           },
         ),
       );
-      // Navigator.of(context).pop(
+
+      print(response.body);
+      print(response.statusCode);
+
+      if (!context.mounted) {
+        return;
+      }
+      Navigator.of(context).pop();
       //   GroceryItem(
       //       id: DateTime.now().toString(),
       //       name: _enteredName,
